6c07f84ce819a91974b44b2f106f2c0bd600bd85
BogaDevelopment <bogadevelopment@gmail.com>
2025-07-04 15:26:42 -0300

    33 - Add the save button in the Recipe view

--- composeApp/src/commonMain/kotlin/com/bogadevelopment/heirloomrecipes/recipe/ui/RecipeView.kt
+++ composeApp/src/commonMain/kotlin/com/bogadevelopment/heirloomrecipes/recipe/ui/RecipeView.kt
@@ -17,17 +17,29 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bogadevelopment.heirloomrecipes.login.ui.CustomText
@@ -41,14 +53,53 @@ data class RecipeScreen(val id: Int)
 fun RecipeScreen(viewModel: RecipeViewModel, onBack: () -> Unit) {
     Scaffold(
         topBar = {
-            ToolBar(
+            ToolBarActions(
                 viewModel.state?.tittle ?: "No tittle",
-                Icons.AutoMirrored.Filled.ArrowBack
-            ) { onBack() }
+                Icons.AutoMirrored.Filled.ArrowBack,
+                { onBack() },
+                {}
+            )
         },
     ) { innerPadding ->
         Content(innerPadding, viewModel)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ToolBarActions(text : String, icon : ImageVector, onClick : () -> Unit, onAction : () -> Unit){
+    TopAppBar(
+        title = {CustomText(text, MaterialTheme.typography.titleMedium, MaterialTheme.colorScheme.onBackground, modifier = Modifier.padding(start = 5.dp))},
+        colors = TopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.secondary,
+            scrolledContainerColor = Color.Yellow,
+            navigationIconContentColor = Color.Cyan,
+            titleContentColor = Color.White,
+            actionIconContentColor = Color.Black
+        ),
+        navigationIcon = {
+            Icon(
+                imageVector = icon,
+                contentDescription = "Drawer Icon",
+                modifier = Modifier.clickable { onClick() }.padding(start = 5.dp),
+                tint = MaterialTheme.colorScheme.onBackground
+
+            )
+        },
+        actions = {
+            var expanded by remember { mutableStateOf(false) }
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = "Options",
+                modifier = Modifier.clickable { expanded = !expanded }.padding(end = 5.dp),
+                tint = MaterialTheme.colorScheme.onBackground)
+            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false}, modifier = Modifier.background(MaterialTheme.colorScheme.surface)){
+                DropdownMenuItem(
+                    text = { Text("Delete", style = MaterialTheme.typography.bodyMedium, color = MaterialTheme.colorScheme.onBackground) },
+                    onClick = { expanded = false; {onAction}})
+            }
+        }
+    )
 }
 
 
