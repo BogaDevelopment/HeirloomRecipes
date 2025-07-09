18e694e7236221899d019db0fa5c8fab4fa9c3ae
BogaDevelopment <bogadevelopment@gmail.com>
2025-07-09 19:13:19 -0300

    4 - Add the LoginViewModel logic

--- app/src/main/java/com/bogadevelopment/heirloomrecipes/features/login/ui/LoginScreen.kt
+++ app/src/main/java/com/bogadevelopment/heirloomrecipes/features/login/ui/LoginScreen.kt
@@ -13,9 +13,11 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bogadevelopment.heirloomrecipes.core.ui.components.CustomText
 import com.bogadevelopment.heirloomrecipes.core.ui.components.CustomTittle
@@ -64,19 +66,20 @@ fun Body(
     onLoggedIn : () -> Unit,
 ) {
 
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     Column(modifier = modifier) {
         VerticalSpacer(40)
         CustomTittle("Welcome", MaterialTheme.typography.titleLarge, MaterialTheme.colorScheme.onBackground)
         VerticalSpacer(60)
-        Field("", {  }, "Email", false, "email")
+        Field(uiState.email, { viewModel.onEmailChanged(it) }, "Email", uiState.error != null, "email")
         VerticalSpacer(10)
-        Field("", {  }, "Password", false ,"password")
+        Field(uiState.password, { viewModel.onPasswordChanged(it) }, "Password", uiState.error != null ,"password")
         VerticalSpacer(5)
         CustomText("Forgot password ?", MaterialTheme.typography.bodyMedium,
             MaterialTheme.colorScheme.onBackground , Modifier.align(Alignment.End))
         VerticalSpacer(20)
-        GeneralButton("Log in", 20, Modifier, {},true)
+        GeneralButton("Log in", 20, Modifier, { onLoggedIn() },uiState.isLoginEnable)
         VerticalSpacer(40)
         Row(modifier = Modifier.align(Alignment.CenterHorizontally)){
             CustomText("Don't have an account?", MaterialTheme.typography.bodyMedium, MaterialTheme.colorScheme.onBackground)
--- app/src/main/java/com/bogadevelopment/heirloomrecipes/features/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/bogadevelopment/heirloomrecipes/features/login/ui/LoginViewModel.kt
@@ -1,7 +1,53 @@
 package com.bogadevelopment.heirloomrecipes.features.login.ui
 
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class LoginViewModel : ViewModel() {
 
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState : StateFlow<LoginUiState> = _uiState
+
+    fun onEmailChanged(email: String) {
+        _uiState.update { state ->
+            state.copy(email = email)
+        }
+        verifyLogin()
+    }
+
+    fun onPasswordChanged(password: String) {
+        _uiState.update { state ->
+            state.copy(password = password)
+        }
+        verifyLogin()
+    }
+
+    private fun verifyLogin(){
+        if(isEmailValid() && isPasswordValid() && fieldsNotEmpty()){
+            _uiState.update {
+                it.copy(isLoginEnable = true)
+            }
+        }else{
+            _uiState.update {
+                it.copy(isLoginEnable = false)
+            }
+        }
+    }
+
+    private fun isEmailValid(): Boolean = Patterns.EMAIL_ADDRESS.matcher(_uiState.value.email).matches()
+    private fun isPasswordValid(): Boolean = _uiState.value.password.length >= 8
+    private fun fieldsNotEmpty() : Boolean = _uiState.value.email.isNotEmpty() && _uiState.value.password.isNotEmpty()
+
 }
+
+
+data class LoginUiState (
+    val email : String = "",
+    val password : String = "",
+    val loggedIn : Boolean = false,
+    val isLoginEnable : Boolean = false,
+    val error : String? = null
+)
